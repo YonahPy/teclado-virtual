b49a56634740a70a9cdc5e6c02fcfcba5c3822d0
YonahPy <jrjonas487@gmail.com>
2023-07-31 20:21:35 -0300

    arruma essas funções

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+function handleSpecialKeys(key) {
+    const capsLockKey = document.querySelector('[data-key="caps"]');
+    const textoDigitado = document.getElementById('textoDigitado');
+  
+    switch (key) {
+        case "caps":
+            capsLockKey.classList.toggle('active');
+            textoDigitado.classList.toggle('caps-on');
+            break;
+  
+        case "space":
+            textoDigitado.textContent += " ";
+            break;
+    
+        case "backspace":
+            textoDigitado.textContent = textoDigitado.textContent.slice(0, -1);
+            break;
+    
+        case "enter":
+            textoDigitado.textContent += "\n";
+            break;
+    
+        // Outras teclas especiais aqui
+    
+        default:
+            break;
+    }
+  }
+  
+
 function pressKey(element) {
+   const key = element.getAttribute('data-key');
+    console.log(key);
+  
+  handleSpecialKeys(key);
     element.classList.add('pressed');
     setTimeout(() => {
          element.classList.remove('pressed');
@@ -37,8 +71,15 @@ const teclas = document.querySelectorAll('.key');
 
 teclas.forEach(tecla => {
     tecla.addEventListener('click', function() {
-    const textoDigitado = document.getElementById('textoDigitado');
-    textoDigitado.textContent += this.textContent; // Adiciona o texto da tecla clicada ao texto já existente na tela
+    const key = this.getAttribute('data-key');
+    
+    
+    if (["caps", "space", "backspace", "enter"].includes(key)) {
+        handleSpecialKeys(key);
+    } else {
+        const textoDigitado = document.getElementById('textoDigitado');
+        textoDigitado.textContent += this.textContent;
+    }
     });
 });
 
